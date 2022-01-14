30cfa1925ce781329027e413ab5ebb82cc5b6bbe
Adam Bowles <github@adambowl.es>
2022-01-14 14:19:05 +0000

    Using a creative commons logo

--- src/components/App/component.tsx
+++ src/components/App/component.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from '../../logo.png';
 import './styles.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>From the Ashes hello world deploy</p>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>From the Ashes</p>
       </header>
     </div>
   );
